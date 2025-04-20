52cb3bc27a8cfdb7799567d9d9429e6446c85889
Giselle Rosales <gr539@drexel.edu>
2025-04-19 23:33:29 -0400

    basic ui and sample text

--- listApp/App.js
+++ listApp/App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component, useState} from 'react'; 
+import {StyleSheet, View, Text, Button, Alert, TextInput, FlatList} from 'react-native'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Flex extends Component {
+
+  _onButtonPress = () => {
+    console.log("Button is pressed!"); 
+    Alert.alert('You have pressed the button!!!'); 
+  }
+
+  render() {
+    return (
+      <View style={[styles.container]}>
+        <View style={{flex:1, flexDirection:'column', alignItems:'left', padding:20}}>
+          <Text style={[styles.heading]}>MyTinerary</Text>
+
+          {/* Typing and adding an item to list */}
+          <Text style={styles.subheading}>ACTIVITY</Text>
+          <TextInput style={[styles.TextInput]}>What do you want to experience?</TextInput>
+          <Button
+            title="+ Add item"
+            onPress={this._onButtonPress}
+            color='steelblue'
+          />
+        </View>
+
+        {/* Displaying the list */}
+        <View style={{flex:1, flexDirection:'column', alignItems:'left', padding:20}}>
+          <Text style={[styles.subheading]}>TO DO</Text>
+          <FlatList
+            data={[
+              {name: 'activity1'},
+              {name: 'activity2'},
+              {name: 'activity3'},
+            ]}
+            renderItem={
+              ({item}) => <Text style={styles.item}>{item.name}</Text>
+            }
+          />
+        </View>
+      </View>
+    ); 
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    flexDirection: 'column', 
+    padding: 20,
+  }, 
+  heading: {
+    fontWeight: 'bold', 
+    fontSize: 42,
+    paddingBottom: 20,
+  }, 
+  subheading: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 5,
+    backgroundColor: '#f6f6f6',
+    marginTop: 5,
+    marginBottom: 10,
+    // inner text styles
+    color: 'grey',
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontStyle: 'italic',
+  },
+  item: {
+    fontSize: 16,
+    paddingBottom: 10,
   },
 });
