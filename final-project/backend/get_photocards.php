10ebfdd6897de14d862676c66a6303281971c667
Giselle Rosales <gr539@drexel.edu>
2025-06-10 20:17:01 -0400

    database retrieval working

---
+++ final-project/backend/get_photocards.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+// Enable CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Set to JSON
+header("Content-Type: application/json");
+
+// Connect to SQLite
+$db = new SQLite3('photocards.db');
+
+// Query entries
+$results = $db->query("SELECT * FROM binder");
+
+$photocards = [];
+
+while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+    $photocards[] = $row;
+}
+
+echo json_encode($photocards);
+?>
